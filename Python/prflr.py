d644c6ab12ed2171ced4359e552678612fd3c7f6
Alexey Spiridonov <forspall@gmail.com>
2014-01-16 14:31:53 +0300

    Update prflr.py

--- Python/prflr.py
+++ Python/prflr.py
@@ -18,11 +18,11 @@ import threading
 PY3 = sys.version_info[0] == 3
 
 class Prflr:
-    def __init__(self, api_key, source=''):
+    def __init__(self, api_key, source):
         self.addr = ('prflr.org', 4000)
-        self.source = source if source else socket.gethostname()
+        self.source = source[:32]
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.key = api_key
+        self.key = api_key[:32]
         self.timers = {}
         self.lock = threading.RLock()
 
@@ -49,11 +49,11 @@ class Prflr:
 
     def send(self, timer, time, info, thread_id):
         msg = '|'.join([str(thread_id)[:32],
-                        self.source[:32],
+                        self.source,
                         timer[:48],
                         str(time)[:16],
                         info[:32],
-                        self.key[:32]])
+                        self.key])
         if PY3:
             self.socket.sendto(bytes(msg, 'utf-8'), self.addr)
         else:

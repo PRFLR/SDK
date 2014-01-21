b694626d53743b765ae3963fa0ab5008a92ed1ce
Alexey Spiridonov <forspall@gmail.com>
2014-01-21 10:41:55 +0300

    Update test.java

--- Java/test.java
+++ Java/test.java
@@ -1,27 +1,27 @@
 public class Program {
 	public static void main(String[] args){
 		try {
-			PRFLP.init("192.168.1.45-testAppn", "testKey");
+			PRFLR.init("192.168.1.45-testAppn", "testKey");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		PRFLP.overflowCount = 50;
-		PRFLP.begin("mongoDB.save");
+		PRFLR.overflowCount = 50;
+		PRFLR.begin("mongoDB.save");
 		for(int i = 0; i < 10; i++) {
-			PRFLP.begin("mongoDB.save step" + i);
+			PRFLR.begin("mongoDB.save step" + i);
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
 			try {
-				PRFLP.end("mongoDB.save step" + i, "step " + i);
+				PRFLR.end("mongoDB.save step" + i, "step " + i);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		try {
-			PRFLP.end("mongoDB.save", "Good!");
+			PRFLR.end("mongoDB.save", "Good!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
